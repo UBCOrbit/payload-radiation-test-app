4dde4611fb93a89a173bf5283dcebfb9a0bc25cc
Curtis <mathcurt@gmail.com>
2017-09-20 21:34:23 -0700

    added basic functions

--- app/src/main/java/ubcorbit/org/testapp/services/StupidService.java
+++ app/src/main/java/ubcorbit/org/testapp/services/StupidService.java
@@ -1,18 +1,71 @@
 package ubcorbit.org.testapp.services;
+
+import java.util.Random;
+
 /**
  * Created by curtis on 20/09/17.
  */
 
 public class StupidService {
-    public static int incrementCounter(){
-        int n = 1000;
+    public static int incrementCounter(int n){
+        int total = 0;
         for(int i = 0;i < n;i++){
-            thread.sleep()
+            total += n;
         }
-        return n;
+        return total;
     }
-    public static int allocate_and_check_ram(){
+    public static int allocate_and_check_ram(int size,int time){
+        byte[] array = new byte[size];
+        for(int i = 0; i < size; i++){
+            array[i] = 0;
+        }
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        return 0;
+        int errors = 0;
+        for(int i = 0; i < size; i++){
+            if (array[i] != 0){
+                errors++;
+            }
+        }
+
+        for(int i = 0; i < size; i++){
+            array[i] = 0b1111111;
+        }
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for(int i = 0; i < size; i++){
+            if (array[i] != 0b1111111){
+                errors++;
+            }
+        }
+
+        return errors;
+
+    }
+    public static int random_accesses(int array_size, int accesses, int delay){
+        Random rng = new Random();
+
+        int[] array = new int[array_size];
+        for (int i = 0; i < array_size; i++){
+            array[i] = i;
+        }
+
+        int errors = 0;
+        for (int i = 0; i < accesses; i++){
+            int next = rng.nextInt();
+            if (array[next] != next){
+                errors++;
+            }
+        }
+
+        return errors;
     }
 }
