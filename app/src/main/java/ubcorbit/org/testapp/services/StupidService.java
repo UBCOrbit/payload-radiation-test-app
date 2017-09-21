6f423d44330492852f0f0600f41fbf934c14976d
Curtis <mathcurt@gmail.com>
2017-09-20 20:52:51 -0700

    preliminary service

---
+++ app/src/main/java/ubcorbit/org/testapp/services/StupidService.java
@@ -0,0 +1,18 @@
+package ubcorbit.org.testapp.services;
+/**
+ * Created by curtis on 20/09/17.
+ */
+
+public class StupidService {
+    public static int incrementCounter(){
+        int n = 1000;
+        for(int i = 0;i < n;i++){
+            thread.sleep()
+        }
+        return n;
+    }
+    public static int allocate_and_check_ram(){
+
+        return 0;
+    }
+}
