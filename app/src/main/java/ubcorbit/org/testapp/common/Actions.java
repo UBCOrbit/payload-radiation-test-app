316406e394a0519cfb9812b2ec113cc10dfee21d
Bulat Nugmanov <bbnugmanov@ualberta.ca>
2017-09-22 11:43:21 -0700

    added new completion actions

--- app/src/main/java/ubcorbit/org/testapp/common/Actions.java
+++ app/src/main/java/ubcorbit/org/testapp/common/Actions.java
@@ -8,5 +8,8 @@ public class Actions {
     public static final String RANDOM_ACCESS_DONE = "ram-done";
     public static final String ALLOC_CHECK_DONE = "alloc-done";
     public static final String INCREMENT_CHECK_DONE = "increment-done";
+    public static final String CARD_READ_DONE = "card-read-done";
+    public static final String CARD_WRITE_DONE = "card-write-done";
+    public static final String RAM_FILLER_DONE = "ram-filler-done";
 
 }
