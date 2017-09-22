176ab1624a28ef7de31345c20a2a227766f41a88
Bulat Nugmanov <bbnugmanov@ualberta.ca>
2017-09-22 12:21:35 -0700

    moved test status receiver to startup / handler service

--- app/src/main/java/ubcorbit/org/testapp/MainActivity.java
+++ app/src/main/java/ubcorbit/org/testapp/MainActivity.java
@@ -1,19 +1,37 @@
 package ubcorbit.org.testapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import ubcorbit.org.testapp.services.StartupService;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * This is a dummy activity required for the app services
+     * to function independently.
+     *
+     * IMPORTANT: Upon each (re)install this activity must be run
+     * at least once for the the manifest to be processed, and the
+     * services and receivers to be registered.
+     */
+
+    // NOTE: all class tags have to begin with "orbit" for easier log-filtering
     private static final String TAG = "orbitMainAc";
     private static String RECORD_NAME = "main-activity.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         Log.i(TAG, "onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // StartupService handles all the test runs
+        Intent i = new Intent(this, StartupService.class);
+        startService(i);
+
     }
 
 }
--- app/src/main/java/ubcorbit/org/testapp/services/StartupService.java
+++ app/src/main/java/ubcorbit/org/testapp/services/StartupService.java
@@ -1,9 +1,14 @@
 package ubcorbit.org.testapp.services;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+import ubcorbit.org.testapp.common.Actions;
 
 /**
  * Created by bulat on 22/09/17 for TestApp
@@ -14,20 +19,44 @@ public class StartupService extends Service {
 
     @Override
     public void onCreate() {
+
         super.onCreate();
         Log.i(TAG, "onCreate()");
+
+        TestStatusReceiver receiver = new TestStatusReceiver();
+        LocalBroadcastManager broadcastManager = LocalBroadcastManager.getInstance(this);
+        broadcastManager.registerReceiver(receiver, new IntentFilter(Actions.ALLOC_CHECK_DONE));
+
+        Intent i = new Intent(this, AllocateCheckService.class);
+        i.putExtra(AllocateCheckService.ITAG_DELAY, 1000);
+        i.putExtra(AllocateCheckService.ITAG_ARRAY, 10000);
+        startService(i);
 
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i(TAG, "onStartCommand()");
-        return super.onStartCommand(intent, flags, startId);
+        return Service.START_REDELIVER_INTENT; // todo: is this the best strategy?
     }
 
     @Override
     public IBinder onBind(Intent arg0) {
         return null;
+    }
+
+    private class TestStatusReceiver extends BroadcastReceiver {
+
+        private final String TAG = "TestStatusReceiver";
+
+        private TestStatusReceiver() {}
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i(TAG, "onReceive()");
+            // todo: switch on intent action to restart the appropriate service
+        }
+
     }
 
 }
